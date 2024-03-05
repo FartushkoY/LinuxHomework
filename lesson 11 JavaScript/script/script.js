5d916f5bbfd25f015fddcbb088c38ac55b68ed0c
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-03-05 11:22:55 +0100

    Added files

---
+++ lesson 11 JavaScript/script/script.js
@@ -0,0 +1,61 @@
+// console.log('hello word')
+// document.write('hello')
+
+// let a = 3
+// console.log(a)
+
+// a = 6
+// console.log(a)
+
+// let q,w,e,t;q;
+// q=10
+// w=15
+
+// console.log(q+w)
+
+// const c=5
+// console.log(c)
+
+// const userName='Yevgeniia'
+
+
+// const number1 = Number(prompt('Введите число'))
+// const number2 = +prompt('число 2')
+
+// console.log(`результат умножения ${number1 * number2}`)
+// console.log(`результат деления ${number1 / number2}`)
+// console.log(`результат вычитания ${number1 - number2}`)
+// console.log(`результат сложения ${number1 + number2}`)
+// console.log(`результат возведения в степень ${number1 ** number2}`)
+// console.log(`возвращает остаток ${number1 % number2}`)
+
+// const num = 24
+// const text = 'hello'
+
+// console.log(typeof (text))
+// console.log(typeof (num))
+
+// const firstName = prompt('Введите имя')
+// const lastName = prompt('Введите фамилию')
+// const age = prompt('Ваш возраст')
+
+// Конкатенация (сложение) строк
+
+// console.log('Привет '+ firstName + ' ' + lastName + ' Тебе '+  age + ' лет. ' + 'Добро пожаловать')
+
+// document.write('Привет '+ firstName + ' ' + lastName + ' Тебе '+  age + ' лет. ' + 'Добро пожаловать')
+
+// alert('')
+// confirm('')
+// prompt('')
+
+// Интерполяция строк
+// console.log(`Привет ${firstName} ${lastName}! Тебе ${age} лет.`)
+
+const r = 12
+const g = 34
+const b = 14
+
+console.log('rgb(' + r +', ' + g + ', ' + b + ')')
+
+console.log(`rgb( ${r}, ${g}, ${b})`)
